0d3e9c3ead0e766dbaf9b17eb774a3069c1e7ed0
Brayan Legarda Villegas <brayanlegarda33@gmail.com>
2023-08-29 11:25:13 -0500

    initial commit

---
+++ src/main/java/com/oracle/alura/conversordemoneda/logica/ConversorDeMoneda.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.oracle.alura.conversordemoneda.logica;
+import com.oracle.alura.conversordemoneda.gui.PantallaConversorDeMoneda;
+import com.oracle.alura.conversordemoneda.gui.PantallaConversorDeMedidasLongitud;
+import com.oracle.alura.conversordemoneda.gui.PantallaPrincipal;
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author Brayan Legarda Villegas
+ */
+public class ConversorDeMoneda {
+
+    public static void main(String[] args) throws IOException {
+        
+        
+        PantallaPrincipal pantalla = new PantallaPrincipal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+      
+    }
+}
